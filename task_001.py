7b45b81c9bea8777095f4899dd1a7ce5d7a6bd5c
Andrey Pashnin <kapashnin@Andreys-MacBook-Pro.local>
2018-08-26 09:28:32 +0200

    v.001.01

--- task_001.py
+++ task_001.py
@@ -1,39 +1,16 @@
-# --------------------------------------------------------------
-# Part I. Learn what is "Stack" about.
+# Task 001. 
 # 
-# (You can use Google, Youtube or any other way. 
-# For example, wikipedia: 
-# https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
-# 
-# In short: 
-# "stack" is a collection of something with 2 principal operations:
-#   1. PUSH (adds one more something to the collection)
-#   2. POP  (removes the most recently added something)
+# 1. Create a file "a.py".
+# 2. Put the following code in this file:
+#    - Create an "empty" class with name "A".
+#      Empty class is a class that has minimum stuff in it, 
+#      but is still considered to be a class by Python.
 
-# --------------------------------------------------------------
-# Part II. Why do you need it.
-#
-# It is one of most common data structures. It can be used to solve many problems.
-# For example, you have a task: "I give you a word. You need to reverse this word."
-#
-# Can you write a program which does this? 
-# Yes, do it. No? Yes, but lazy? Keep reading...
+class A:
+    def __init__(self):
+        pass
 
-# --------------------------------------------------------------
-# Part III. How to use it.
-#
-# Python doesn't know "stack", it knows "list". Close enough. Let's use it. 
-# Run python in bash and try the following:
-# https://docs.python.org/2/tutorial/datastructures.html#using-lists-as-stacks
-# 
-# What you need to remember is that python's LIST has:
-#   1. APPEND (same as stack's push) 
-#   2. POP (same as stack's pop)
 
-# Now, rememeber what "loop" is? 
-# Look here: https://docs.python.org/2/tutorial/controlflow.html#for-statements
-# 
-# What you need to understand is:
 
 
 
