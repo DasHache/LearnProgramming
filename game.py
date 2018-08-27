4e24fbba1d54c8b51d3a76eb750d86e0c8e65ac4
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2018-08-27 22:49:22 +0200

    test

---
+++ game.py
@@ -0,0 +1,7 @@
+from world import World
+
+class Game:
+    def __init__(self):
+        print "I am a constructor of the class Game"
+
+game = Game()
---
+++ world.py
@@ -0,0 +1,4 @@
+class World:
+    def __init__(self):
+        print "I am a constructor of the class World"
+world= World()
