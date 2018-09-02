5908fe3811b5ae29c657b13a9f461cb2321701d9
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2018-09-02 17:46:05 +0200

    i do the thing

--- game.py
+++ game.py
@@ -4,12 +4,12 @@ from world import World
 class Game:
     def __init__(self):
         print "I am a constructor of the class Game"
+        world = World()
+        a = Tk.Tk()
+        canvas = Tk.Canvas(a, width=300, height=200)
+        canvas.create_rectangle(20, 20, 260, 160,  fill="light blue")
+        canvas.pack()
+        a.mainloop()
 
 game = Game()        
-world = World()
-a = Tk.Tk()
-canvas = Tk.Canvas(a, width=300, height=200)
-canvas.create_rectangle(20, 20, 260, 160,  fill="light blue")
-canvas.pack()
-a.mainloop()
  
