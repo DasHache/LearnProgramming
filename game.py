7a43f61611887e09d90dd9a36dcc1ffef3ef5094
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2018-09-02 12:48:57 +0200

    i do the thing

--- game.py
+++ game.py
@@ -1,8 +1,15 @@
+import Tkinter as Tk
 from world import World
 
 class Game:
     def __init__(self):
         print "I am a constructor of the class Game"
 
+game = Game()        
 world = World()
-game = Game()
+a = Tk.Tk()
+canvas = Tk.Canvas(a, width=300, height=200)
+canvas.create_rectangle(20, 20, 260, 160,  fill="light blue")
+canvas.pack()
+a.mainloop()
+ 
