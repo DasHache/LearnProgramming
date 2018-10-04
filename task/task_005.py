6bd7486b2df84f8e2f0d67af65adffe141ffd08a
kowaraj <kowaraj4stuff@gmail.com>
2018-10-04 21:22:32 +0200

    Add task_005.py

---
+++ task/task_005.py
@@ -0,0 +1,20 @@
+# Task 005.
+
+# Have a look at images in the folder "./images/"
+# There you have 2 images:
+#   tank_000.png
+#   tank_001.png
+
+# Right now your `python game.py` creates something 
+# that looks like tank_000.png.
+
+# You task is to make it create something that looks like tank_001.png.
+# Namely, your canvas should contain:
+#  1. a mountain (big black rectangle)
+#  2. a tank on the mountain (smaller red rectagle)
+#  3. a barrel attached to the middle of the tank (long narrow rectangle)
+
+# That's it. 3 points.
+.
+
+
