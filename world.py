47e07d8fd9fc5f52a74a2aa323b3c1afe8a286f7
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2019-02-21 15:30:49 +0100

    its alive

--- world.py
+++ world.py
@@ -7,11 +7,14 @@ class World:
         print "I am a constructor of the class World"
         self.a = Tk.Tk()
         canvas = Tk.Canvas(self.a, width=300, height=200)
+        self.canvas = canvas
+        self.x = 150
+        self.x1 = 130
         canvas.create_rectangle(20, 20, 260, 160,  fill="light blue")
         canvas.create_rectangle(20, 160, 80, 100,  fill="black")
         canvas.create_rectangle(50, 120, 100, 80,  fill="red")
         canvas.create_rectangle(80, 100, 150, 100,  fill="brown")
-        canvas.create_oval(150, 110, 130, 90, fill="yellow")
+        self.id_charge = canvas.create_oval(self.x, 110, self.x1, 90, fill="yellow")
         canvas.pack()
         
         
@@ -25,8 +28,22 @@ class World:
 
     def hi(self):
         print "Hello world"
+       	current_color=self.canvas.itemcget(self.id_charge, 'fill')
+        print 'color = ', current_color 
+        
+        if current_color == 'yellow':
+            self.canvas.itemconfigure(self.id_charge, fill='red')
+        if current_color == 'red':
+            self.canvas.itemconfigure(self.id_charge, fill='blue')
+        if current_color == 'blue':
+            self.canvas.itemconfigure(self.id_charge, fill='red')
+            
+        self.canvas.coords(self.id_charge, self.x+10, 110, self.x1+10, 90)
+        self.x = self.x+10
+        self.x1 = self.x1+10
         t = Timer(1.0, self.hi)
         t.start()
+        
 
     
     
