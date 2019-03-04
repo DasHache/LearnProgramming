30e39cc98de2c861a0c2644c8867154a3a92032b
kowaraj <kowaraj4stuff@gmail.com>
2019-03-04 22:13:06 +0100

    Add file 'handle.py'

---
+++ handle.py
@@ -0,0 +1,19 @@
+class Handle:
+
+    def __init__(self, world):
+        self.canvas = world.canvas
+        self.canvas.bind("<Button-1>", self.handle_mouse)
+        self.canvas.bind("<Key>", self.handle_key)
+        self.canvas.focus_set()
+
+    def handle_key(self, event):
+        print "You pressed a key!"
+        if event.char == u'1':
+            print " ... key is '1'"
+        elif event.char == u'a':
+            print " ... key is 'a'"
+        else:
+            print " ... key is unknown"
+
+    def handle_mouse(self, event):
+        print "You clicked a mouse button."
