58f7251d9d946527161f4ca441f4180e2eb7f228
Andrey Pashnin <apashnin@cern.ch>
2019-04-05 20:08:23 +0200

    Add class Tank

---
+++ tank.py
@@ -0,0 +1,8 @@
+class Tank:
+
+    def __init__(self, the_world_I_live_in, my_initial_position_x, my_initial_position_y):
+        print "Hi, I am an instance of the Tank class"
+        self.w = the_world_I_live_in
+        self.x = my_initial_position_x
+        self.y = my_initial_position_y
+    
