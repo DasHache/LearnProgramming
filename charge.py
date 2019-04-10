9d58189098719aa2149bbd43d1206cc7bdcf09d2
Darya Chekeres <sha@Daryas-MacBook-Air.local>
2019-04-10 16:59:28 +0200

    added second charge

--- charge.py
+++ charge.py
@@ -24,7 +24,9 @@ class Charge:
         y1 = self.y -z
         y2 = self.y +z
         self.id_on_canvas = self.t.w.canvas.create_oval(x1, y1, x2, y2, fill="yellow")
-        
+
+        self.id_on_canvas2 = self.t.w.canvas.create_oval(x1 + 10, y1 + 10, x2 + 10 , y2 + 10, fill="y\
+ellow")
         
 
 
